16b10ac4183f85bd73f52edee81e971aba1504e0
caokhoi2702 <ngcaokhoi2702@gmail.com>
2024-09-27 11:53:39 +0700

    add event when id not found and cannot convert id to ObjectID

--- app/main.py
+++ app/main.py
@@ -54,6 +54,7 @@ class Post(BaseModel):
 #             return idx
 
 # request Get method url: "/"
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -69,17 +70,26 @@ def get_posts():
 
 @app.get("/posts/{id}")
 def get_post(id: str):
-    post = client["main_db"]["Posts"].find_one(ObjectId(id))
-
-    if not post:
+    try:
+        post = client["main_db"]["Posts"].find_one(ObjectId(id))
+        if not post:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f'post with id: {id} was not found')
+    except Exception as error:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'post with id: {id} was not found')
+                            detail=str(error))
+    
     post["_id"] = str(post["_id"])
     return {"data": post}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    client["main_db"]["Posts"].insert_one(dict(post))
+    try:
+        client["main_db"]["Posts"].insert_one(dict(post))
+    except Exception as error:
+        print(error)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=str(error))
     get_posts = client["main_db"]["Posts"].find({})
     posts = []
     for document in get_posts:
@@ -89,7 +99,16 @@ def create_post(post: Post):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_posts(id: str):
-    client["main_db"]["Posts"].delete_one({"_id": ObjectId(id)})
+    try:
+        delete_post = client["main_db"]["Posts"].delete_one({"_id": ObjectId(id)})
+        if not delete_post.deleted_count :
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f'post with id: {id} was not found')
+    except Exception as error:
+        print(error)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=str(error))
+    
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
@@ -97,10 +116,19 @@ def update_post(id: str, post: Post):
     update_operation = { '$set' : 
         dict(post)
     }
-    update_post = client["main_db"]["Posts"].update_one({'_id': ObjectId(id)}, update_operation)
-    if (update_post == None):
+    try:
+        update_post = client["main_db"]["Posts"].update_one({'_id': ObjectId(id)}, update_operation)
+        if not update_post.modified_count :
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f'post with id: {id} was not found')
+        
+    except Exception as error:
+        print(error)
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {id} does not exist")
+                            detail=str(error))
+
     post = client["main_db"]["Posts"].find_one(ObjectId(id))
     post["_id"] = str(post["_id"])
     return {"Data": post}
+    
+    
